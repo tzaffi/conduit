41c341c8a3af9e2d6204ecc15a21f99035d88716
Zeph Grunschlag <zeph@algorand.com>
2023-08-13 15:00:03 -0500

    wip

--- examples/make_just.py
+++ examples/make_just.py
@@ -20,6 +20,7 @@ substitutions = {
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
+parser.add_argument("--just-out-file", default="Justfile.tmp")
 parser.add_argument("--name")
 parser.add_argument("--networks")
 parser.add_argument("--go-algorand")
@@ -28,10 +29,12 @@ parser.add_argument("--private-data-node")
 
 
 args = parser.parse_args()
+templ_args = vars(args)
+file_out = templ_args.pop("just_out_file")
 
 # Update the substitutions with the arguments
 substitutions.update({
-    key: vars(args)[k] for key in substitutions if vars(args)[(k := key.lower())] is not None
+    key: templ_args[k] for key in substitutions if templ_args[(k := key.lower())] is not None
 })
 
 # Open the template and substitute the placeholders with actual values
@@ -40,6 +43,6 @@ with open('Justfile.tmpl', 'r') as f:
     result = template.substitute(substitutions)
 
 # Write the result to the output file
-with open('Justfile2', 'w') as f:
+with open(file_out, 'w') as f:
     f.write(result)
 
